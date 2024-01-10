4a4d85e223190ef2ef9914e046e5b784bf2d10df
Ruymatheus <87478633+Ruymatheus@users.noreply.github.com>
2024-01-09 23:32:00 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -4,7 +4,7 @@ namespace DesafioPOO.Models
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
-            // Construtor que chama o construtor da classe base
+            
         }
 
         public override void InstalarAplicativo(string nomeApp)
